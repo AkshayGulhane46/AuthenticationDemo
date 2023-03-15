8b40d4a21c895b5c17e48d16c02a68007e4c9514
Akshay Gulhane <argulhane10246@gmail.com>
2023-03-15 23:21:42 +0530

    commit1503231

--- controllers/interview_detail_Controller.js
+++ controllers/interview_detail_Controller.js
@@ -7,13 +7,21 @@ module.exports.interview_detail = async function(req, res){
     //console.log(req.query.id);
     let interview = await Interview.findById(req.query.id);
     let students = await Student.find({});
-    let interview_students = await Interview.find
+    let interview_students = interview.students;
+    let studentss = await Interview.find({}).populate('students')
+    let studentsss = studentss.students;
+
+    console.log(studentsss);
+
+
+   // console.log(interview_students);
     //console.log(students);
     //console.log("company id is",interview);
     return res.render('interview-detail',{
         company_name : interview.company_name,
         company_id : interview._id,
-        student_detail:students
+        student_detail:students,
+        interview_student : studentss
     }
     );
 }
